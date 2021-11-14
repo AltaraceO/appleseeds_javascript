dfe7353fa29c63a664ee9d28753c23e523a4957f
AltaraceO <orialtarace@gmail.com>
2021-11-14 15:48:50 +0200

    populate and loop-over maps

---
+++ fundamentals/8.1_mapLoop_altarace.js
@@ -0,0 +1,17 @@
+const firstObj = {
+  name: "bob",
+};
+const secondObj = {
+  name: "bertha",
+};
+const thirdObj = {
+  name: "blanch",
+};
+
+const firstMap = new Map();
+
+firstMap.set(firstObj, 23).set(secondObj, 24).set(thirdObj, 25);
+
+for (let [key, val] of firstMap) {
+  console.log(key.name, val);
+}
