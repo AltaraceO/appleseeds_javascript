b70bcb7151124171e4611e11c6b5676e38c81372
AltaraceO <orialtarace@gmail.com>
2021-11-16 15:59:43 +0200

    re-visiting forEach

--- fundamentals/12.3_SchoolMethods_altarace.js
+++ fundamentals/12.3_SchoolMethods_altarace.js
@@ -43,13 +43,45 @@ const school = {
     });
   },
   assignStudent: function (subject, id) {
+    // x = student object
+    let x = this.findPerson("students", id);
+
+    // itetereting all teaches
     this.teachers.forEach(function (el) {
+      // el-> specific teacher object
+      //loop e.subject.length times
       for (let i = 0; i < el.subjects.length; i++) {
-        if (el.subjects[i] === subject) {
-          if (el.capacityLeft !== 0) {
-            console.log(el.capacityLeft);
-            el.students.push(findPerson("students", id));
+        //check teacher has the subject property
+        //check teacher has capacity left in capacity property
+        console.log(el.subjects);
+        if (el.subjects[i] === subject && el.capacityLeft > 0) {
+          //el.student -> array
+          el.students.push(x);
+          el.capacityLeft--;
+          // return;
+          console.log(el);
+          return;
+        }
+      }
+      console.log("Sorry, no available teachers left");
+    });
+  },
+  assignTeacher: function (newSub, id) {
+    // X - is the teacher object that I will use
+    let x = this.findPerson("teachers", id);
+    // loop over the teachers objects
+    this.teachers.forEach(function (el) {
+      // find if their ID matches
+      if (el.id === id) {
+        // loop over the subjects array in teacher obj
+        console.log(el.id);
+        for (let i = 0; i < el.subjects.length; i++) {
+          // see if there no match for NewSub
+          if (el.subjects[i] !== newSub) {
+            console.log(newSub);
+            el.subjects.push(newSub);
             console.log(el);
+            return;
           }
         }
       }
@@ -57,5 +89,10 @@ const school = {
   },
 };
 
-console.log(school.findPerson("teachers", 1));
-console.log(school.assignStudent("history", 10));
+// console.log(school.findPerson("teachers", 1));
+// console.log(school.assignStudent("chemistry", 10));
+// console.log(school.assignStudent("history", 13));
+// console.log(school.assignStudent("history", 13));
+console.log(school.assignTeacher("P.E.", 1));
+console.log(school.assignTeacher("Trig", 1));
+// console.log(school.teachers[0].students)
