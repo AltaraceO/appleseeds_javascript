9236fd3600ab7de079b7745a2b2e1586969dc595
AltaraceO <orialtarace@gmail.com>
2021-11-17 23:04:14 +0200

    end of day

--- fundamentals/15.4_average_altarace.js
+++ fundamentals/15.4_average_altarace.js
@@ -1,10 +1,13 @@
-function​ calcAverage​(arr){
-    var​ ​ sum​ ;
-    for​ (​ var​ ​ i ​ = ​ 0 ​ ; ​ i ​ ​ < ​ ​ arr​.length; ​ i ​ ++​ ){
-    sum​ ​ +=​ ​ arr​ [ ​ i ​ ];
-    }
-    return​ ​ sum​ ;
-    }
-    calcAverage​([​ 85​ ,90​ ,92​ ]);
+function calcAverage(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(calcAverage([85, 90, 92]));
+// 1 invalid or unexpected token, red sign
 
-    // 1 invalid or unexpected token, red sign 
+// 2 the sum was undefined manking it a NaN with every loop.
+
+// the returned sum was not devided by the length of the array .
