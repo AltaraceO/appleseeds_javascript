14226d6cdf7ff09d743bbc40f11205149486a9aa
AltaraceO <orialtarace@gmail.com>
2021-11-21 22:58:24 +0200

    end of day

---
+++ review/myFind.js
@@ -0,0 +1,19 @@
+const theArr2 = [10, 30, 40, 60, 80, 1000];
+
+// function equality(arrVal, num) {
+//   return arrVal === num;
+// }
+
+function myFind(arr) {
+  for (let i = 0; i < arr.lenght; i++) {
+    console.log(arr);
+
+    // if (callback(arr[i], num)) {
+    //   return `yes`;
+    // }
+  }
+}
+
+myFind(theArr2);
+
+// console.log(findResult);
