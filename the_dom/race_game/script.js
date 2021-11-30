069b84bf3d067ae6daff084fcf21a6892c057ccd
AltaraceO <orialtarace@gmail.com>
2021-11-30 23:29:40 +0200

    end of day

---
+++ the_dom/race_game/script.js
@@ -0,0 +1,18 @@
+const player1 = document.querySelector("#player1-race > td");
+const player2 = document.getElementById("player2-race");
+const allTD = document.querySelector("div");
+
+let tdStyle = window.getComputedStyle(player1);
+
+const backgroundUrl = tdStyle;
+
+console.log(backgroundUrl);
+
+window.addEventListener("keyup", function (e) {
+  console.log(e.key);
+
+  if (e.key === "ArrowDown") {
+    console.log(player1.nextSibling);
+    player1.nextElementSibling.style = backgroundUrl;
+  }
+});
