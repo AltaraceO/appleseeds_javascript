c087878c112adb82371151c96b25d908ae0ba6a1
AltaraceO <orialtarace@gmail.com>
2021-12-07 19:40:26 +0200

    async await

--- promise/291_async_await.js
+++ promise/291_async_await.js
@@ -12,31 +12,3 @@ async function getId() {
     console.log(error);
   }
 }
-
-// const getRecipe = (recipeID) => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(
-//       (ID) => {
-//         const recipe = {
-//           title: "Fresh tomato pasta",
-//           publisher: "Pinchas Hodadad",
-//         };
-//         resolve(`${ID}: ${recipe.title}`);
-//       },
-//       1500,
-//       recipeID
-//     );
-//   });
-// };
-
-// getIDs()
-//   .then((IDs) => {
-//     console.log(IDs);
-//     return getRecipe(IDs[2]);
-//   })
-//   .then((recipe) => {
-//     console.log(recipe);
-//   })
-//   .catch((error) => {
-//     console.log("It is an error!");
-//   });
